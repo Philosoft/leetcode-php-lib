35ce45691a34e257dc11ab2b4ffd30868ffd4915
thephilosoft <thephilosoft@gmail.com>
2023-03-22 15:42:39 -0400

    fix: namespsaces

--- src/SinglyLinkedList/ListNode.php
+++ src/SinglyLinkedList/ListNode.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace thephilosoft\SinglyLinkedList;
+namespace thephilosoft\leetcode\lib\SinglyLinkedList;
 
 final class ListNode
 {
@@ -10,5 +10,31 @@ final class ListNode
         public readonly int $val,
         public ?ListNode $next = null
     ) {
+    }
+
+    /** @param int[] $input */
+    public static function fromArray(array $input): ?ListNode
+    {
+        $dummy = new self(666);
+        $head = $dummy;
+        foreach ($input as $number) {
+            $head->next = new self($number);
+            $head = $head->next;
+        }
+
+        return $dummy->next;
+    }
+
+    /** @return int[] */
+    public function toArray(): array
+    {
+        $result = [];
+        $h = $this;
+        while ($h !== null) {
+            $result[] = $h->val;
+            $h = $h->next;
+        }
+
+        return $result;
     }
 }
--- tests/SinglyLinkedList/ListNodeTest.php
+++ tests/SinglyLinkedList/ListNodeTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace tests\SinglyLinkedList;
 
 use PHPUnit\Framework\TestCase;
-use thephilosoft\SinglyLinkedList\ListNode;
+use thephilosoft\leetcode\lib\SinglyLinkedList\ListNode;
 
 class ListNodeTest extends TestCase
 {
@@ -23,5 +23,32 @@ class ListNodeTest extends TestCase
         self::assertSame(3, $three->val);
         self::assertNotNull($three->next);
         self::assertSame(4, $three->next->val);
+    }
+
+    public function testCreationFromArray(): void
+    {
+        self::assertNull(ListNode::fromArray([]));
+
+        $head = ListNode::fromArray([1]);
+        self::assertNotNull($head);
+        self::assertSame(1, $head->val);
+        self::assertNull($head->next);
+
+        $head = ListNode::fromArray([1, 2]);
+        self::assertNotNull($head);
+        self::assertSame(1, $head->val);
+        self::assertNotNull($head->next);
+        self::assertSame(2, $head->next->val);
+        self::assertNull($head->next->next);
+    }
+
+    public function testToArray(): void
+    {
+        self::assertSame([1, 2, 3], ListNode::fromArray([1, 2, 3])?->toArray());
+
+        $head = ListNode::fromArray([1, 2, 3]);
+
+        self::assertSame([2, 3], $head->next?->toArray());
+        self::assertSame([3], $head->next->next?->toArray());
     }
 }
